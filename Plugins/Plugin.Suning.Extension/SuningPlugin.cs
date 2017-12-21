85f49a91e1421f8924a1570fd88c807ab606dbef
chenwenguang <c-wenguang@kuaiyixing.com.cn>
2017-12-21 15:49:37 +0800

    苏宁URL解析完毕

--- Plugins/Plugin.Suning.Extension/SuningPlugin.cs
+++ Plugins/Plugin.Suning.Extension/SuningPlugin.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.IO;
+using System.Linq;
 using NTCPMessage.EntityPackage.Products;
 using NTCPMessage.EntityPackage.Arguments;
 using NTCPMessage.EntityPackage;
-
 using ShoppingPeeker.Plugins;
 
 namespace Plugin.Suning.Extension
@@ -39,6 +40,120 @@ namespace Plugin.Suning.Extension
                 return dir;
             }
         }
+
+        /// <summary>
+        /// 解析搜索地址
+        /// </summary>
+        /// <param name="webArgs"></param>
+        /// <returns></returns>
+        public override ResolvedSearchUrlWithParas ResolveSearchUrl(BaseFetchWebPageArgument webArgs)
+        {
+            ResolvedSearchUrlWithParas resultUrl = new ResolvedSearchUrlWithParas();
+
+
+            StringBuilder sbSearchUrl = new StringBuilder("https://search.suning.com/@###@/");
+
+
+
+            #region 品牌
+         
+            if (null != webArgs.Brands && webArgs.Brands.Count > 0)
+            {
+                //1 当前平台的品牌
+                var currentPlatformBrands = webArgs.Brands.Where(x => x.Platform == SupportPlatformEnum.Suning);
+                if (currentPlatformBrands.Any())
+                {
+                 
+
+                    //多个品牌直接将id拼接为字符串，国美家的 是4位加密码进行的拼接组
+                    string brandNames = string.Join(";", currentPlatformBrands.Select(x => x.BrandName));
+
+                   
+                    sbSearchUrl.Append("&hf=brand_Name_FacetAll:").Append(brandNames);
+                     
+                }
+
+                //2 非当前平台的品牌--选择其中的一个 作为关键词 分割
+                var otherPlatformBrands = webArgs.Brands.FirstOrDefault(x => x.Platform != SupportPlatformEnum.Suning);
+                if (null != otherPlatformBrands)
+                {
+                    webArgs.KeyWord += " " + otherPlatformBrands.BrandName;
+                }
+            }
+            #endregion
+
+
+
+            #region  属性标签
+            if (null != webArgs.TagGroup)
+            {
+                //1 当前平台的
+                var currentPlatformTag = webArgs.TagGroup.Tags.Where(x => x.Platform == SupportPlatformEnum.Suning);
+                if (currentPlatformTag.Any())
+                {
+
+                    #region 分类
+                    var catIdTag = currentPlatformTag.FirstOrDefault(x => x.FilterFiled == "ci");
+                    if (null != catIdTag)
+                    {
+                        sbSearchUrl.Append("&ci=").Append(catIdTag.Value);
+                    }
+                    #endregion
+
+                    //https://search.suning.com/羽绒服/&iy=0&sc=0&cf=solr_13696_attrId:收腰型;常规&st=0#search-path-box
+                    string attrIds = string.Join(";", currentPlatformTag.Select(x => x.Value));
+                    sbSearchUrl.Append("&cf=")
+                        .Append(currentPlatformTag.First().FilterFiled)
+                        .Append("_attrId:")
+                        .Append(attrIds);
+
+                }
+
+                //2 其他平台的tag 作为关键词的一部分
+                var otherPlatformTag = webArgs.TagGroup.Tags.FirstOrDefault(x => x.Platform != SupportPlatformEnum.Suning);
+                if (null != otherPlatformTag)
+                {
+                    webArgs.KeyWord += " " + otherPlatformTag.TagName;
+                }
+            }
+
+         
+            #endregion
+
+            #region 关键词
+            sbSearchUrl.Replace("@###@", webArgs.KeyWord);//将关键词的占位符 进行替换
+            #endregion
+
+            #region  排序
+            if (null == webArgs.OrderFiled)
+            {
+                sbSearchUrl.Append("&st=0");//默认综合排序
+            }
+            else
+            {
+                sbSearchUrl.Append("&st=").Append(webArgs.OrderFiled.FieldValue);//默认综合排序
+            }
+            #endregion
+
+            #region  筛选-价格区间
+            #endregion
+
+            #region  页码
+
+            sbSearchUrl.Append("&cp=").Append(webArgs.PageIndex + 1);
+
+            #endregion
+            # region 杂项
+            sbSearchUrl.Append("&iy=0");
+            sbSearchUrl.Append("&sc=0");
+ 
+            sbSearchUrl.Append("#second-filter");
+            
+            #endregion
+            resultUrl.Url = sbSearchUrl.ToString();
+            return resultUrl;
+        }
+
 
         /// <summary>
         /// 执行内容解析
